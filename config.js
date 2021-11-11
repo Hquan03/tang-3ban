b21a9d012f54f628592ae8fccb5f83db3818f888
Hquan03 <87995874+Hquan03@users.noreply.github.com>
2021-11-11 19:55:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle:Xin chèo! ,
-    introDesc: `Gửi 3 tình iu của Ngô, Hải và Quân
+    introTitle: Xin chèoo! ,
+    introDesc: `Gởi choa bạn Quềnh aka bé mèo bíu siu đáng iu
     Ấn vô dưới để cóa nhiều điều bất ngờ nhé `,
-    btnIntro: 'đây nè 3 má',
-    title: ' Xin lũi vì chúng tôi đã phạm phải những sai lầm tồi tệ với các bạn =((( ',
-    desc: ' hãy tha lỗi cho bọn tôi những người đàn ông lầm lỡ :( Ấn có để đồng ý và nhận 1 kèo lẩu nướng  ',
-    btnYes: ' Okii đoòng ý tha lỗi <3  ',
-    btnNo: ' không cút đi 3 th tệ bạc ',
-    question:' 3 bạn nữ đáng iu có muốn chúng tôi làm gì tặng các bạn khôm ',
-    btnReply: ' Gửi 3 người đàn ông đẹp trai nhất ',
-    reply: ' không các cậu là tuyệtt vời nhất heheh ',
-    mess: ' Chúng tôi biết mà hehe! tối nay bọn mình call bonding nha ',
-    messDesc: ' tối nay bọn mình call bonding nha ',
+    btnIntro: 'đây nè má',
+    title: ' Yay chúc mừng sinh nhật tủi mừi tắm cụa bé mèo méo meo nhaa =))) ',
+    desc: ' Chúc bạn Quỳnh tuổi mới xinh đẹp mạnh khoẻ học roải và đỗ AEISEC nhaa.
+Không biết là tui coá thể mời banh Quềnh đi chơi 1 bủi khum nhị 😳🙄😳 
+ ',
+    btnYes: ' Okii đoòng ý nhaa :> ',
+    btnNo: ' không tự đi 1 mình ik (đố ấn được zô đây keke) ',
+    question:' Bạn Quềnh coá mún tui làm gì tặng sinh nhật khum nè, hoặc là coá mún nói rì vứi tui hem hêh ',
+    btnReply: ' day ne ',
+    reply: ' cám ưn hquan đẹp trai đáng iu cute nhiều nha ',
+    mess: ' Tôi biết mà hehe! Anw chúc mừng sinh nhật nghenn mong là bọn mình sẽ nói chuyện nhiều hơn ha ',
+    messDesc: ' Happy birthdayyy tủi 18 siêu đẹp nha!!! ',
     btnAccept: 'Okiiiii lun ',
     messLink: 'https://www.facebook.com/messages/t/4006798229445982'
 }
